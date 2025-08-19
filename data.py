968a80bc8d55021d4617791fe7a8865252b362d2
Ankitha616 <ankithashetty616@gmail.com>
2025-08-19 11:25:03 +0530

    defined function to find length

--- data.py
+++ data.py
@@ -1,8 +1,15 @@
 from django.template.defaultfilters import length
+from numpy.lib.function_base import iterable
 
 str_ ="selenium"
 
-length=0
-for i in str_:
-    length +=1
-print(length)
+def finding_length(iterable):
+    length=0
+    for i in iterable():
+        length +=1
+    print(length)
+
+finding_length(str_)
+
+
+
