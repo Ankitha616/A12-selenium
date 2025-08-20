b52f707de0564c0b810263b6630a8024784e3e55
Ankitha616 <ankithashetty616@gmail.com>
2025-08-20 08:17:42 +0530

    defined the class sample

--- data.py
+++ data.py
@@ -3,12 +3,10 @@ from numpy.lib.function_base import iterable
 
 str_ ="selenium"
 
-def finding_length(iterable):
-    length=0
-    for i in iterable():
-        length +=1
-    print(length)
+class Sample:
 
-finding_length(str_)
-finding_length([1,2,3,4,5,6,7,8])
-finding_length("akshitha")
+    def finding_length(self,iterable):
+        length=0
+        for i in iterable():
+            length +=1
+        print(length)
